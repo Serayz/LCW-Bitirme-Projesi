cb1f8dd937cada79886cf2b2287929cbe344d8f2
Serayz <ezgiserayb@gmail.com>
2022-06-26 14:00:44 +0300

    üye ol sayfası eklendi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +11,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+
     public static void main(String[] args) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications", "--disable-blink-features=AutomationControlled");
@@ -22,7 +24,45 @@ public class Main {
         driver.get("https://www.lcwaikiki.com/");
         driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
         //loginTest(driver);
+
+        WebElement girisYap = driver.findElement(By.xpath("//*[@id=\"header__container\"]/header/div[2]/div[3]/div/span/div/a"));
+        girisYap.click();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement uyeOl = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/div/div/form/a"));
+        uyeOl.click();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement elementEmail = driver.findElement(By.xpath("//*[@id=\"register\"]/div/div/div/form/div[1]/input"));
+        elementEmail.click();
+        elementEmail.sendKeys("hyd@hyd.com");
+
+        WebElement elementPassword = driver.findElement(By.xpath("//*[@id=\"register\"]/div/div/div/form/div[2]/div/input"));
+        elementPassword.click();
+        elementPassword.sendKeys("ABC_DEF*123");
+
+        WebElement phone = driver.findElement(By.xpath("//*[@id=\"register\"]/div/div/div/form/div[3]/input"));
+        phone.click();
+        phone.sendKeys("019121180");
+
+        WebElement checkBox1 = driver.findElement(By.xpath("//*[@id=\"register\"]/div/div/div/form/div[5]/label[1]/span"));
+        checkBox1.click();
+
+        WebElement checkBox2 = driver.findElement(By.xpath("//*[@id=\"member-privacy-approve-container\"]/div[1]/input"));
+        checkBox2.click();
+
     }
+
     private static void loginTest(WebDriver driver) {
         WebElement girisYap = driver.findElement(By.xpath("//*[@id=\"header__container\"]/header/div[2]/div[3]/div/span/div/a"));
         girisYap.click();
@@ -33,12 +73,12 @@ public class Main {
             e.printStackTrace();
         }
 
-        WebElement elementEposta = driver.findElement(By.name("email"));
-        elementEposta.click();
-        elementEposta.sendKeys("hyd@hyd.com");
-        WebElement elementSifre = driver.findElement(By.name("password"));
-        elementSifre.click();
-        elementSifre.sendKeys("951951");
+        WebElement elementEmail = driver.findElement(By.name("email"));
+        elementEmail.click();
+        elementEmail.sendKeys("testautomationesb@gmail.com");
+        WebElement elementPassword = driver.findElement(By.name("password"));
+        elementPassword.click();
+        elementPassword.sendKeys("Auto5keuro.");
 
         try {
             Thread.sleep(2000);
